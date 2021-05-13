485ccebe2d29cde02449219b855cd1e0c67ef8dc
Max <mmaj.classroom@gmail.com>
2021-05-13 17:52:57 +0200

    file to base64 T.T

--- maysAngular/src/app/core/pop-components/pop-posted/pop-posted.component.ts
+++ maysAngular/src/app/core/pop-components/pop-posted/pop-posted.component.ts
@@ -11,6 +11,7 @@ import {PopService} from '../pop.service';
 export class PopPostedComponent implements OnInit {
   popPosted: FormGroup;
   connected: boolean;
+  fileB64: string;
 
   constructor(private popService: PopService, private auth: AuthService, private fb: FormBuilder) {
     this.popPosted = fb.group({
@@ -33,15 +34,32 @@ export class PopPostedComponent implements OnInit {
     const user = this.auth.isAuthenticated;
 
     if (user) {
+
       const content = this.popPosted.value.fileContent.match(/([^\\/]+)$/);
       content.shift();
       const name = content[0];
       const form = this.popPosted.value;
       Object.assign(form, {fileName: name});
-
+      form.fileContent = this.fileB64;
       this.popService.postPop(form).subscribe((data) => {
         console.log(this.popPosted);
       });
     }
+  }
+
+  onTheFly(event): void {
+    console.log(event);
+    this.toBase64(event.target.files[0]).then(fileB => {
+      this.fileB64 = fileB;
+    });
+  }
+
+  toBase64(file): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = error => reject(error);
+    });
   }
 }
