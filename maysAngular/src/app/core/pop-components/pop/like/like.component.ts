2f15bccb3958b0e064b8f33a36dc059baad9b672
Max <mmaj.classroom@gmail.com>
2021-06-13 19:54:16 +0200

    did stuff

--- maysAngular/src/app/core/pop-components/pop/like/like.component.ts
+++ maysAngular/src/app/core/pop-components/pop/like/like.component.ts
@@ -1,8 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {LikeService} from './like.service';
 import {AuthService} from '../../../../guard/auth-service/auth.service';
-import {FormBuilder, FormGroup} from '@angular/forms';
-// import {PopService} from '../../pop.service';
 
 @Component({
   selector: 'app-like',
@@ -11,21 +9,15 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class LikeComponent implements OnInit {
   eventBox: boolean;
-  likeForm: FormGroup;
   @Input() postId: string;
   @Input() isLiked: boolean;
   id: string;
   isAuth: boolean;
   @Input() numberLike: number;
 
-  constructor(private likeService: LikeService, private auth: AuthService, private fb: FormBuilder, /*private popService: PopService*/) {
-    this.likeForm = this.fb.group({
-      id: ['']
-    });
-  }
+  constructor(private likeService: LikeService, private auth: AuthService) {}
 
   ngOnInit(): void {
-    this.likeForm.controls.id.setValue(this.postId);
     this.auth.isAuth$.subscribe((data) => {
       this.isAuth = data;
     });
